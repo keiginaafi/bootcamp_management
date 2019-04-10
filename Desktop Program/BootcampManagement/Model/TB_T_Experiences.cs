df1021c8f0c38c4f9d8fcd5340a754a29c3153ec
Hilmi <di.prongz@gmail.com>
2019-04-10 20:37:57 +0700

    add model : Model/TB_M_Experience.cs, Model/TB_T_Experiences.cs

---
+++ Desktop Program/BootcampManagement/Model/TB_T_Experiences.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootcampManagement.Model
+{
+    class TB_T_Experiences : BaseModel
+    {
+        public virtual TB_M_Experience TB_M_Experiences { get; set; }
+        public virtual TB_T_CV TB_T_CVs { get; set; }
+    }
+}
--- Desktop Program/BootcampManagement/MyContext.cs
+++ Desktop Program/BootcampManagement/MyContext.cs
@@ -33,6 +33,8 @@ namespace BootcampManagement
         public DbSet<TB_M_Major> TB_M_Majors { get; set; }
         public DbSet<TB_M_Organization> TB_M_Organizations { get; set; }
         public DbSet<TB_T_Organization> TB_T_Organizations { get; set; }
+        public DbSet<TB_M_Experience> TB_M_Experiences { get; set; }
+        public DbSet<TB_T_Experiences> TB_T_Experiences { get; set; }
 
     }
 }
