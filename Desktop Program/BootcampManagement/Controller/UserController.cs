96453d7c72d9d7aa2a4a488058f3397e1306aad3
Mrp4m <pmbyn.ofc@gmail.com>
2019-04-10 14:30:59 +0700

    Add Migration User

---
+++ Desktop Program/BootcampManagement/Controller/UserController.cs
@@ -0,0 +1,44 @@
+﻿using BootcampManagement.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BootcampManagement.Model;
+using BootcampManagement.SaveData;
+
+namespace BootcampManagement.Controller
+{
+    class UserController : IUser
+    {
+        MyContext myContext = new MyContext();
+        //SaveData Newi = new SaveData();
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TB_M_User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TB_M_User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(TB_M_User item)
+        {
+            myContext.TB_M_Users.Add(item);
+            //return valid.validation();
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, TB_M_User item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Desktop Program/BootcampManagement/SaveData/SaveData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootcampManagement.SaveData
+{
+    class SaveData
+    {
+        bool status = false;
+        MyContext myContext = new MyContext();
+
+        public SaveData(MyContext _myContext)
+        {
+            myContext = _myContext;
+        }
+
+        //var result = Convert.ToBoolean(myContext.SaveChanges());
+        public bool saveData()
+        {
+            var result = (myContext.SaveChanges());
+            if (result > 0)
+            {
+                Console.WriteLine("Succses");
+                status = true;
+            }
+            else
+            {
+                Console.WriteLine("NotSuccses");
+            }
+            return status;
+        }
+    }
+}
