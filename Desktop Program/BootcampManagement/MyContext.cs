4ec0345e22021434e34731afffbd31588f0cdf42
Hilmi <di.prongz@gmail.com>
2019-04-10 16:20:26 +0700

    add new model: Model/TB_M_Employee.cs, Model/TB_M_Role.cs

--- Desktop Program/BootcampManagement/MyContext.cs
+++ Desktop Program/BootcampManagement/MyContext.cs
@@ -16,11 +16,13 @@ namespace BootcampManagement
         }
 
         public DbSet<TB_M_District> TB_M_Districts { get; set; }
+        public DbSet<TB_M_Role> TB_M_Roles { get; set; }
         public DbSet<TB_M_Province> TB_M_Provinces { get; set; }
         public DbSet<TB_M_Region> TB_M_Regions { get; set; }
         public DbSet<TB_M_Religion> TB_M_Religions { get; set; }
         public DbSet<TB_M_User> TB_M_Users { get; set; }
         public DbSet<TB_M_Village> TB_M_Villages { get; set; }
+        public DbSet<TB_M_Employee> TB_M_Employees { get; set; }
 
     }
 }
